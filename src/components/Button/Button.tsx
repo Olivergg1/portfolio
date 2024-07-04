729e18662c2a0a12e786cfe905fa4a1ff5e1c548
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-07-04 18:26:23 +0200

    fix className for Button + new ExecutableButton

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,31 @@
 import { ButtonHTMLAttributes } from 'react'
 
 import './Button.css'
+import { useExecute } from '../../hooks/console.hooks'
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  href?: boolean
+}
 
-export default function Button(props: ButtonProps) {
+export default function Button({ href, ...props }: ButtonProps) {
   return (
-    <button {...props} className={`std-button ${props.className}`}>
+    <button
+      {...props}
+      className={`std-button ${href && 'href'} ${props.className ?? ''}`}>
       {props.children}
     </button>
+  )
+}
+
+interface ExecuteButtonProps extends ButtonProps {
+  e: string
+}
+
+export function ExecuteButton({ e, ...props }: ExecuteButtonProps) {
+  const execute = useExecute()
+  return (
+    <Button {...props} onClick={() => execute(e)}>
+      {props.children}
+    </Button>
   )
 }
