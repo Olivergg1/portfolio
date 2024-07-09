2a00fdee6d32db91f6cbba18c3dce1beb4225356
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-07-10 00:24:29 +0200

    add mock helpers for mocking and updating default imports

--- src/helpers/mock.helpers.tsx
+++ src/helpers/mock.helpers.tsx
@@ -5,3 +5,33 @@ export function asMock(target: any) {
 
   return target as jest.Mock
 }
+
+export function mockDefault<T>(module: string, mockValue: any) {
+  jest.mock(module, () => ({
+    __esModule: true,
+    default: mockValue,
+  }))
+
+  const restoreMock = () => restoreDefaultMock(module)
+  const populateMock = (componentModule: string, mockValue: any) =>
+    populateDefaultMock(module, componentModule, mockValue)
+
+  return { populateMock, restoreMock }
+}
+
+export function populateDefaultMock(
+  module: string,
+  componentModule: string,
+  mock: any
+) {
+  jest.doMock(module, () => ({
+    __esModule: true,
+    default: mock,
+  }))
+
+  return require(componentModule).default
+}
+
+export function restoreDefaultMock(module: string) {
+  jest.dontMock(module)
+}
