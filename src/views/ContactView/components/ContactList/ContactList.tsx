342c864122ad27e4387de9b35eaea9491cc6971c
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-07-10 00:27:10 +0200

    add error handling for contact view

--- src/views/ContactView/components/ContactList/ContactList.tsx
+++ src/views/ContactView/components/ContactList/ContactList.tsx
@@ -1,14 +1,18 @@
 import { Contacts } from '@/types/contact.types'
 import ContactItem from '../ContactItem/ContactItem'
+import Error from '@/components/Error/Error'
 
 interface ContactListProps {
   contacts: Contacts
 }
 
 export default function ContactList({ contacts }: ContactListProps) {
+  const contactEntries = Object.entries(contacts)
+
   return (
     <ul>
-      {Object.entries(contacts).map(([key, value]) => (
+      {contactEntries.length === 0 && <Error>No contacts yet</Error>}
+      {contactEntries.map(([key, value]) => (
         <ContactItem key={key} contact={value} />
       ))}
     </ul>
