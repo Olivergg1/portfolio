8454a135ca42cc07d28ef888e01bd5aaceacb32d
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-07-14 00:10:11 +0200

    add header to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Console from '@/components/Console/Console'
+import Header from '@/components/Header/Header'
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Console />
     </div>
   )
