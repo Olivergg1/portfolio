e5af6a45dbf9d8780739db12de76e89774d812c4
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-08-22 19:26:05 +0200

    separate config into components

--- src/config/about.config.ts
+++ src/config/about.config.ts
@@ -1,18 +1,22 @@
 import AboutBuilder from '@/classes/about.classes'
 import resource from '@/resources/About.resource.json'
 
-const builder = new AboutBuilder()
-
-builder
-  .addSubtitle('Who I am')
+export const AboutComponent = new AboutBuilder()
   .addField(resource.introduction)
-  .addField(resource.education)
-  .addField(resource.experience)
+  //.addField(resource.education)
+  //.addField(resource.experience)
   .addField(resource.skills)
+  .addExec('How I built my portfolio', 'about -page portfolio')
+  .addExec('My most recent project', 'about -page recent')
+  .build()
+
+export const AboutProject = new AboutBuilder()
+  .addSubtitle('How I built my portfolio')
+  .addField(resource.portfolio)
+  .build()
+
+export const AboutRecent = new AboutBuilder()
   .addSubtitle('My most recent project')
   .addField(resource.ryggar)
   .addLink('Check out my game!', 'https://olivergg1.github.io/Ryggar/')
-  .addSubtitle('How was this portfolio built?')
-  .addField(resource.portfolio)
-
-export const AboutComponent = builder.build()
+  .build()
